d022a83edf2bf258d7a1e4df258b569e2be7e0c1
Michelle Rosenthal <127794503+mtrinhsenthal@users.noreply.github.com>
2025-11-29 19:17:18 -0800

    add another test file

--- src/tourbus.py
+++ src/tourbus.py
@@ -1,5 +1,8 @@
 # Bubble removal using Tourbus algorithm
 
+# current state: figuring out what to do about twin nodes (reverse direction)
+# should they still be separated or can they count as one node now?
+
 import graph
 from graph import Node
 
@@ -21,7 +24,7 @@ def tourbus(graph, source):
     dist[source] = 0.0
     discovered[source] = True
 
-    # for use as tiebreaker during dijkstra
+    # for use as tiebreaker in priority queue
     tie_counter = itertools.count()
     pq = [(0.0, next(tie_counter), source)]
 
@@ -33,18 +36,24 @@ def tourbus(graph, source):
 
         # precompute dictionary of multiplicities
         multiplicities = dict(Counter(u[0].out_edges))
-        print(f"multiplicities! {multiplicities}")
 
         # explore all neighbors (outgoing) of current vertex
         for neighbor in multiplicities:
             # edge cost is the length of s(B) divided by the multiplicity of the arc leading from A to B
             edge_weight = len(neighbor.descriptor) / multiplicities[neighbor]
-            
+            new_dist = cur_dist + edge_weight
+
             # print("hello node", u)
             # print("hey neighbor", neighbor)
-            # print(dist[neighbor])
-            # if dist[u] + edge_weight < dist[neighbor]:
-            #     dist[neighbor] = dist[u] + edge_weight
+            # if not discovered[neighbor]:
+            #     discovered[neighbor] = True
+            #     dist[neighbor] = new_dist
+            #     parent[neighbor] = u
+                # push onto heap (remember the counter tiebreaker)
+            # else:
+                # neighbor has already been discovered, so backtrack!
+
+                # decide which path to keep
 
 
     return dist
