a1aa9c15a8c2cd4830babb76eef341d95feaf83b
Ashley Osuna <ashleyosuna@uvic.ca>
2025-11-30 17:36:33 -0800

    added function to determine whether two paths are similar, also added a simple test for tour bus

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import sys
+import numpy as np
 
 def reverse_complement(seq: str):
     copy = seq[::-1]
@@ -9,6 +10,57 @@ def canonical_form(seq: str):
     rev_seq = reverse_complement(seq)
     # return min(seq, rev_seq)
     return ((seq, 1) if seq < rev_seq else (rev_seq, -1))
+
+def compare_sequences(u, v):
+    MAX_GAPS = 3
+    MAX_DIVERGENCE = 0.2
+    INDEL = 0
+    MATCH = 1
+
+    u = u.upper()
+    v = v.upper()
+    
+    # CONSTRUCT MATRIX
+    n, m = len(u) + 1, len(v) + 1
+    network = np.full((n, m), 0, dtype=float)
+
+    # fill first row
+    for i in range(n): network[i][0] = INDEL * i
+
+    # fill first column
+    for j in range(m): network[0][j] = INDEL * j
+
+    # fill matrix row by row
+    for i in range(1, n):
+        for j in range(1, m):
+            network[i][j] = max(
+                network[i-1][j] + INDEL,
+                network[i][j-1] + INDEL,
+                network[i-1][j-1] + (MATCH if u[i-1] == v[j-1] else -MATCH)
+            )
+
+    # BACKTRACK TO CONSTRUCT ALIGNMENT
+    i, j = n - 1, m - 1
+
+    while i > 0 or j > 0:
+        # vertical gap, 'move' along u
+        if network[i][j] == network[i-1][j] + INDEL:
+            i -= 1
+        # horizontal gap, 'move' along v
+        elif network[i][j] == network[i][j-1] + INDEL:
+            j -= 1
+        # if match or mismatch, 'move' along both sequences
+        else:
+            i -= 1
+            j -= 1
+        
+    max_score = network[n-1][m-1]
+    max_len = max(u, v)
+
+    if max_score < max_len - MAX_GAPS: return False
+    if (1 - max_score / max_len) > MAX_DIVERGENCE: return False
+
+    return True
 
 def settings(args):
     if len(args) < 2:
